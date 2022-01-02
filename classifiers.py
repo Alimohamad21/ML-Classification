8ac676d7d751e5dcfb107d1e5a6815dda022626c
Adham <adhamwaelsalem>
2022-01-02 23:27:36 +0200

    Cross-Validation First Attempt

--- classifiers.py
+++ classifiers.py
@@ -3,6 +3,9 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
+import numpy as np
+from sklearn.model_selection import KFold
+
 from dataFunctions import readCsv, split
 
 
@@ -19,10 +22,38 @@ def naiveBayes(X_train, X_test, y_train, y_test):
 
 
 def knn(X_train, X_test, y_train, y_test):
-    tunedK = 5
-    clf = KNeighborsClassifier(n_neighbors=tunedK).fit(X_train, y_train)
-    y_predict = clf.predict(X_test)
-    cal_accuracy(y_test, y_predict)
+    best_acc = [1, 1, 0]
+    for k in range(1, 20):
+        clf = KNeighborsClassifier(n_neighbors=k)
+        acc_list_temp = crossValidate(X_train, y_train, clf)
+        if acc_list_temp[1] > best_acc[2]:
+            best_acc = [k, acc_list_temp[0], acc_list_temp[1]]
+    print(best_acc)
+
+    # y_predict = clf.predict(X_test)
+    # cal_accuracy(y_test, y_predict)
+def crossValidate(X_train, y_train, classifier):
+    acc_list = [0, 0]
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+    for i in range(2, 20):
+        kf = KFold(n_splits=i)
+        print(kf)
+        kf.get_n_splits(X_train)
+        acc = 0
+        counter = 0
+        for train_index, test_index in kf.split(X_train):
+            print("TRAIN:", train_index, "TEST:", test_index)
+            KFX_train, KFX_test = X_train[train_index], X_train[test_index]
+            KFy_train, KFy_test = y_train[train_index], y_train[test_index]
+            classifier.fit(KFX_train, KFy_train)
+            KFy_predict = classifier.predict(KFX_test)
+            acc += accuracy_score(KFy_test, KFy_predict) * 100
+            counter += 1
+        acc = acc/counter
+        if acc > acc_list[1]:
+            acc_list = [i, acc]
+    return acc_list
 
 
 def cal_accuracy(y_test, y_pred):
@@ -37,4 +68,5 @@ def cal_accuracy(y_test, y_pred):
 dataFrame = readCsv()
 X_train, X_test, y_train, y_test = split(dataFrame)
 # decisionTree(X_train, X_test, y_train, y_test)
-naiveBayes(X_train, X_test, y_train, y_test)
+# naiveBayes(X_train, X_test, y_train, y_test)
+knn(X_train, X_test, y_train, y_test)
